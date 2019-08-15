89820f3e7cde589e94fc079a70e3a68411a4249c
Alex Michael <45311289+alexmichaelb@users.noreply.github.com>
2019-08-15 22:44:41 +0100

    Update discord_email_alerts_bot.py
    Bug fixed by adding catch, and except statement. Implemented a login and log out feature for each iteration.

--- discord_email_alerts_bot.py
+++ discord_email_alerts_bot.py
@@ -4,22 +4,17 @@ import email
 import imaplib
 import time
 
-##username and password for Gmail Account
-username = '' 
-password = ''
+username = '' #email
+password = '' #password
 
-##login to gmail via imap
-mail = imaplib.IMAP4_SSL("imap.gmail.com") 
-mail.login(username, password)
-
-##discord bot initialisation and async def
 client = commands.Bot(command_prefix = '.')
+
+##async function runs on program startup
 @client.event
 async def on_ready():
-
-    print("Bot is ready")
     
-    ##defining variables for storing raw email data and count logic
+    ##initialise lists and iteration variables
+    print("Bot is ready")
     previous_subject = ""
     previous_date = ""
     whole_Counter = 0
@@ -27,83 +22,83 @@ async def on_ready():
     from_list = []
     date_list = []
     x = True
-
-    ##runs endless loop to check for new emails
+    
+    ##loop to run main body of code inside async function
     while x == True:
-        n = -1
-
-        ##while size of subject list is not equal to 5
-        while (len(subject_list) != 5):
-
-            ##select inbox, strip down into emails and extract data into variables
-            mail.select("inbox")
-            result, data = mail.uid('search', None, "ALL")
-            inbox_item_list = data[0].split()
-            most_recent = inbox_item_list[n]
-            oldest = inbox_item_list[0]
-            result2, email_data = mail.uid('fetch', most_recent, '(RFC822)')
-            raw_email = email_data[0][1].decode("utf-8")
-            email_message = email.message_from_string(raw_email)
-            from_= email_message['From']
-            subject_ = email_message['Subject']
-            date_ = email_message['Date']
-            
-            ##check for attachments
-            counter = 1
-            for part in email_message.walk():
-                if part.get_content_maintype() == "multipart":
-                    continue
-                filename = part.get_filename()
-                if not filename:
-                     ext = '.html'
-                     filename = 'msg-part-%08d%s' %(counter, ext)
-                counter += 1
-                
-            ##logic to filter out tradingview emails
-            if (("liked" or "is now following") not in (subject_)) and (from_ == "TradingView <noreply@tradingview.com>"):
-                subject_list.append(subject_)
-                from_list.append(from_)
-                date_list.append(date_)
-            n = n-1
-
-        ##logic to check for new alerts and post them to specific channel
-        k=0
-        if (previous_date != date_list[0]):
-            print("New emails!")
-
-            while ((whole_Counter !=0) and (date_list[k] != previous_date)):
-                if (("TradingView Alert:" in subject_list[k]) and ("1H" in subject_list[k])):
-                    channel = client.get_channel() ##fill in channel ID
-                    await channel.send("@everyone **"+subject_list[k]+"**")
-                if(("TradingView Alert:" in subject_list[k]) and ("15M" in subject_list[k])):
-                    channel = client.get_channel() ##fill in channel ID
-                    await channel.send("@everyone **"+subject_list[k]+"**")
-                if (k == 4):
-                    break
-                k=k+1
-
-        ##Assigning variables for iterative logic
-        whole_Counter = whole_Counter + 1
-        previous_subject = subject_list[0]
-        previous_date = date_list[0]
         
-        #Clear lists
-        subject_list.clear()
-        from_list.clear()
-        date_list.clear()
+        ##login with Imap
+        mail = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+        r, d = mail.login(username, password)
+        assert r == 'OK', 'login failed'
+        n = -1
+        
+        ##try and except statement,
+        try:
+            
+            #if decompose inbox into emails and emails into data, put data into lists
+            while (len(subject_list) != 5):
+                mail.select("inbox")
+                result, data = mail.uid('search', None, "ALL")
+                inbox_item_list = data[0].split()
+                most_recent = inbox_item_list[n]
+                oldest = inbox_item_list[0]
+                result2, email_data = mail.uid('fetch', most_recent, '(RFC822)')
+                raw_email = email_data[0][1].decode("utf-8")
+                email_message = email.message_from_string(raw_email)
+                from_= email_message['From']
+                subject_ = email_message['Subject']
+                date_ = email_message['Date']
 
-        #Print iteration count and wait 500 seconds for next check
-        print(whole_Counter)
-        time.sleep(500)
+                counter = 1
+                for part in email_message.walk():
+                    if part.get_content_maintype() == "multipart":
+                        continue
+                    filename = part.get_filename()
+                    if not filename:
+                         ext = '.html'
+                         filename = 'msg-part-%08d%s' %(counter, ext)
+                    counter += 1
 
-client.run('') ##discord client ID
+                ##filter emails to be posted    
+                if (("" or "") not in (subject_)) and (from_ == ""):
+                    subject_list.append(subject_)
+                    from_list.append(from_)
+                    date_list.append(date_)
+                n = n-1
 
+            k=0
+            if (previous_date != date_list[0]):
+                print("New emails!")
 
+                ##checks if email subjects in list meet criteria and posts to channel
+                while ((whole_Counter !=0) and (date_list[k] != previous_date)):
+                    if (("" in subject_list[k]) and ("" in subject_list[k])):
+                        channel = client.get_channel() ##enter specific channel
+                        await channel.send("@everyone **"+subject_list[k]+"**")
+                    if(("TradingView Alert:" in subject_list[k]) and ("15M" in subject_list[k])):
+                        channel = client.get_channel() ##enter specific channel
+                        await channel.send("@everyone **"+subject_list[k]+"**")
+                    if (k == 4):
+                        break
+                    k=k+1
 
+            ##counter iterations and variable assignments for comparisons in next iteration
+            whole_Counter = whole_Counter + 1
+            previous_subject = subject_list[0]
+            previous_date = date_list[0]
 
-
-
-
-
-
-
+            ##clear lists
+            subject_list.clear()
+            from_list.clear()
+            date_list.clear()
+            
+            ##print iteration number, sleep for 5 minutes before checking for new emails
+            print(whole_Counter)
+            time.sleep(300)
+        
+        #catch any exceptions, logout
+        except mail.abort:
+            continue
+        mail.logout()
+            
+client.run('') ##enter discord client number
